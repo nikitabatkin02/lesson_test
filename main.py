fc05fd5d44ae2c2ea56cf23935bdb887941a7548
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 16:47:26 +0500

    12345678

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def guess_the_number():
         except ValueError:
             print("Некорректный ввод. Пожалуйста, введите целое число.")
         except Exception as e:
-            print(f"Произошла непр2345678901едвиденная ошибка: {e}")
+            print(f"Произошла непр234567890едвиденная ошибка: {e}")
 
 
 if __name__ == "__main__":
